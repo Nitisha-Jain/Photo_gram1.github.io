69fafc4121361b9b06aff5b4fa3a48e17ae66bae
Nitisha-Jain <2015pcecsnitisha@poornima.org>
2018-05-28 20:06:21 +0530

    change

--- cloning.js
+++ cloning.js
@@ -2,9 +2,9 @@ $(function(){
     $("#Upload").click(function(){
 
         var person = prompt("Please enter your name");
-        var caption=prompt("Add Capti on");
-        var user_image=prompt("Add User image");
-        var image=prompt("Add image");
+        var caption=prompt("Add Caption");
+        var user_image=prompt("Add image");
+        var image=prompt("Add User image");
        /* var mi= $("#card_clone").clone().appendTo(".row-cards");
         $(".name_c").text(person);
         $("#cap").text(caption);
@@ -13,8 +13,8 @@ $(function(){
         $t1=$("#card_clone").clone();
         $(".name_c",$t1).text(person);
         $("#cap",$t1).text(caption);
-        $("#img",$t1).attr("src",image);
-        $("#p_img",$t1).attr("src",user_image);
+        $("#p_img",$t1).attr("src",image);
+        $("#img1",$t1).css("background-image","url("+user_image+")");
         $t1.appendTo(".row-cards");
        
 
